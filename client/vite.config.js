1d44504471dd41e29651c94b5eb3caf5e997a72c
Sarthak sharma <130988802+sarthaksharma52@users.noreply.github.com>
2024-07-24 13:55:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/assets',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
 })
