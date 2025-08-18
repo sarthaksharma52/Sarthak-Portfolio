0a305be52303e25e169a3440399d8de1db81a3ca
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-08-18 22:46:52 +0530

    resume change

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import pdf from './pdf/Sarthak-Resume-hackerresume (2).pdf';
+import pdf from './pdf/Sarthak_Sharma_Resume_Aug_New.pdf';
 import instagramIcon from '../public/assets/instagram.png';
 import githubIcon from '../public/assets/github.png';
 import linkedinIcon from '../public/assets/linkedin.png';
