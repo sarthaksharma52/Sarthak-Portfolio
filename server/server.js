8c021a6c015c7929e68297caa272992152291920
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-08-19 23:52:19 +0530

    some minor change in server.

--- server/server.js
+++ server/server.js
@@ -47,6 +47,6 @@ app.post('/api/contact', async (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port no. ${PORT}`);
 });
 
